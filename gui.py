f45fca2d47ea55a178f96ff9541aca6031f0d979
Ivan Miguel Malabanan <meguelvan@gmail.com>
2022-11-28 22:34:57 +0800

    modified: parser, scanner, added inputs from lect

--- gui.py
+++ gui.py
@@ -9,60 +9,63 @@ from PyQt5 import QtCore, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1097, 886)
+        MainWindow.resize(1920, 1080)
 
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
         self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(119, 489, 871, 331))
-        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(25, 25, 1485, 819))
 
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
         self.terminalContainer = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+
+        self.terminalContainer.setSizeConstraint(QtWidgets.QLayout.SetMaximumSize)
         self.terminalContainer.setContentsMargins(0, 0, 0, 0)
         self.terminalContainer.setObjectName("terminalContainer")
 
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+
+        self.textBrowser = QtWidgets.QTextBrowser(self.verticalLayoutWidget)
+        self.textBrowser.setMinimumSize(QtCore.QSize(480, 0))
+        self.textBrowser.setObjectName("textBrowser")
+        self.horizontalLayout.addWidget(self.textBrowser)
+
+        self.symbolTable = QtWidgets.QTableWidget(self.verticalLayoutWidget)
+        self.symbolTable.setMinimumSize(QtCore.QSize(480, 0))
+        self.symbolTable.setObjectName("symbolTable")
+        self.symbolTable.setColumnCount(0)
+        self.symbolTable.setRowCount(0)
+        self.horizontalLayout.addWidget(self.symbolTable)
+
+        self.varTable = QtWidgets.QTableWidget(self.verticalLayoutWidget)
+        self.varTable.setMinimumSize(QtCore.QSize(480, 0))
+        self.varTable.setObjectName("varTable")
+        self.varTable.setColumnCount(0)
+        self.varTable.setRowCount(0)
+        self.horizontalLayout.addWidget(self.varTable)
+
+        self.terminalContainer.addLayout(self.horizontalLayout)
         self.pushButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
         self.pushButton.setObjectName("pushButton")
-
         self.terminalContainer.addWidget(self.pushButton)
 
         self.pushButton_2 = QtWidgets.QPushButton(self.verticalLayoutWidget)
         self.pushButton_2.setObjectName("pushButton_2")
-
         self.terminalContainer.addWidget(self.pushButton_2)
+
         self.terminalOutput = QtWidgets.QPlainTextEdit(self.verticalLayoutWidget)
         self.terminalOutput.setObjectName("terminalOutput")
         self.terminalContainer.addWidget(self.terminalOutput)
 
-        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(50, 20, 981, 421))
-        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
-        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-
-        self.textBrowser = QtWidgets.QTextBrowser(self.horizontalLayoutWidget)
-        self.textBrowser.setObjectName("textBrowser")
-        self.horizontalLayout.addWidget(self.textBrowser)
-
-        self.symbolTableWidget = QtWidgets.QTableWidget(self.horizontalLayoutWidget)
-        self.symbolTableWidget.setObjectName("symbolTableWidget")
-        self.symbolTableWidget.setColumnCount(0)
-        self.symbolTableWidget.setRowCount(0)
-        self.horizontalLayout.addWidget(self.symbolTableWidget)
-
-        self.tableWidget_2 = QtWidgets.QTableWidget(self.horizontalLayoutWidget)
-        self.tableWidget_2.setObjectName("tableWidget_2")
-        self.tableWidget_2.setColumnCount(0)
-        self.tableWidget_2.setRowCount(0)
-        self.horizontalLayout.addWidget(self.tableWidget_2)
-
         MainWindow.setCentralWidget(self.centralwidget)
+
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1097, 26))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1920, 26))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
+
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
